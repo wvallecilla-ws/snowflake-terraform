d0cc336ef9815a097736c84c1d04dfccf8d6b1d5
Wilson Vallecilla <wilson.vallecilla@woodside.com.au>
2023-02-17 23:39:26 +0800

    New schema create on main branch

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "snowflake_schema" "demo_schema" {
   comment  = "Schema for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "demo_schema" {
+resource "snowflake_schema" "demo_schema_main" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA_MAIN"
   comment  = "Schema for Snowflake Terraform demo"
