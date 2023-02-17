82dbf919431822583f25c03845806aa6a6001293
Wilson Vallecilla <wilson.vallecilla@woodside.com.au>
2023-02-17 23:34:34 +0800

    Testing changes on main branch

--- main.tf
+++ main.tf
@@ -28,3 +28,9 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA_MAIN"
+  comment  = "Schema for Snowflake Terraform demo"
+}
