eadb762ff03b312db80d0f7a5b0634949d41169e
Wilson Vallecilla <wilson.vallecilla@woodside.com.au>
2023-02-22 18:55:50 +0800

    changes to let it run using github actios

--- main.tf
+++ main.tf
@@ -16,16 +16,16 @@ terraform {
 }
 
 provider "snowflake" {
-  alias            = "sysadmin"
+  /* alias            = "sysadmin"
   username         = var.snowflake_username
   account          = var.snowflake_account
   region           = var.snowflake_region
   private_key_path = var.snowflake_private_key_path
-  role             = "SYSADMIN"
+  role             = "SYSADMIN" */
 }
 
 resource "snowflake_database" "demo_db" {
-  provider = snowflake.sysadmin
-  name     = "DEMO_DB"
-  comment  = "Database for Snowflake Terraform demo"
+  /* provider = snowflake.sysadmin */
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
 }
